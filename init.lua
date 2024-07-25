35053de696d442fcd294f55c8c01079ce3794018
JoowonSuh <sjw2023@gmail.com>
2024-07-25 09:04:21 +0900

    set up colorscheme

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("core.keymaps")
 require("core.options")
 require("config.lazy")
+vim.cmd.color("catppuccin")
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -24,7 +24,6 @@ opt.cursorline = true
 opt.termguicolors = true
 -- opt.background = "dark"
 opt.signcolumn = "yes"
-vim.cmd.colorscheme = "catppuccin"
 
 -- backspace
 opt.backspace = "indent,eol,start"
