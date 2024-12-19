aa43d5a6d798bc9b2466a1685467289ab65f9fa7
JoowonSuh <sjw2023@gmail.com>
2024-12-19 17:43:18 +0900

    Fix: telescope loading timing bug

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -34,13 +34,6 @@ keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer
 
 -- Plugins keymaps
 
--- Telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -2,8 +2,7 @@
 return {
 	-- https://github.com/nvim-telescope/telescope.nvim
 	"nvim-telescope/telescope.nvim",
-	lazy = true,
-	branch = "0.1.x",
+	-- lazy = true,
 	dependencies = {
 		-- https://github.com/nvim-lua/plenary.nvim
 		"nvim-lua/plenary.nvim",
@@ -18,7 +17,30 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
-	config = function()
+	opts = {
+		defaults = {
+			layout_config = {
+				vertical = {
+					width = 0.75,
+				},
+			},
+			path_display = {
+				filename_first = {
+					reverse_directories = true,
+				},
+			},
+			-- path_display = { "smart" },
+			-- mappings = {
+			-- 	i = {
+			-- 		["<C-k>"] = actions.move_selection_previous, -- move to prev result
+			-- 		["<C-j>"] = actions.move_selection_next, -- move to next result
+			-- 		["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+			-- 		["<C-t>"] = trouble_telescope.open,
+			-- 	},
+			-- },
+		},
+	},
+	config = function(_, opts)
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
 		local transform_mod = require("telescope.actions.mt").transform_mod
@@ -31,29 +53,14 @@ return {
 			end,
 		})
 
-		telescope.setup({
-			defaults = {
-				layout_config = {
-					vertical = {
-						width = 0.75,
-					},
-				},
-				path_display = {
-					filename_first = {
-						reverse_directories = true,
-					},
-				},
-				--path_display = { "smart" },
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-						["<C-t>"] = trouble_telescope.open,
-					},
-				},
-			},
-		})
-		-- telescope.load_extension("fzf")
+		require("telescope").setup(opts)
+
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
+	-- telescope.load_extension("fzf")
 }
